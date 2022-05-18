38c6cca49942336acae146d630a3896ae712331a
EmanuelCornea <eandreas186@gmail.com>
2022-05-18 23:15:37 +0300

    I ve deleted everything because we had to start over

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.bla {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-
-
-    opens com.example.bla to javafx.fxml;
-    exports com.example.bla;
-}
