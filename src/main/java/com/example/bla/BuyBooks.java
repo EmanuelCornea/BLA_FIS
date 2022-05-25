3b1f3c17a306b3aa2f6bea9a43037a3f70ab5082
Cojocaru Dumitru-Emilian <jan_emilian@yahoo.com>
2022-05-26 01:30:13 +0300

    A reader is able to buy any book from collection

--- src/main/java/com/example/bla/BuyBooks.java
+++ src/main/java/com/example/bla/BuyBooks.java
@@ -2,6 +2,7 @@ package com.example.bla;
 
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
@@ -9,14 +10,42 @@ import javafx.scene.control.TextField;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import static javax.swing.UIManager.getString;
+
 
 public class BuyBooks extends UserController implements Initializable {
-    public TextField bookNameField;
-    public Button bookNameButton;
+
+    @FXML
+    private TextField tf_bookName;
+
+    @FXML
+    private Button buy_button;
+
+    @FXML
+    private Button cancel_button;
+
+
+
 
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        cancel_button.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                DBUtils.changeScene(actionEvent,"HomePageReader.fxml","Welcome!",username,"reader");
+            }
+        });
+
+        buy_button.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                DBUtils.BuyBooks(tf_bookName.getText());
+            }
+        });
+
+
 
     }
 }
